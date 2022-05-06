64a372814afaad1eb69519da45926266cda022fa
sofian <sofiansalh2305@gmil.com>
2022-05-06 10:33:08 +0200

    add input

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,64 +1,118 @@
-import 'package:english_words/english_words.dart';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
+    const appTitle = 'Form Styling Demo';
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      debugShowCheckedModeBanner: false,
+      title: appTitle,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
+        appBar: HELLO(),
+        body: const MyCustomForm(),
       ),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({ Key? key }) : super(key: key);
+HELLO() {}
 
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  
+class MyCustomForm extends StatelessWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Startup Name Generator'),
-      ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return const Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return ListTile(
-            title: Text(
-              _suggestions[index].asPascalCase,
-              style: _biggerFont,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 50),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Pseudo',
             ),
-          );
-        },
-      ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Email',
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Password',
+              
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              labelText: 'Confirm Password',
+            ),
+            onChanged: (value) {
+              if (getPasswordStrength(value!) < 1) {
+                return 'Not a valid password';
+              }
+              return null;
+            },
+          ),
+        ),
+      ],
     );
   }
+
+
+
+getPasswordStrength(String password) {
+  if (password.isEmpty) return 0;
+  double pFraction;
+  if (RegExp(r'^[0-9]*$').hasMatch(password)) {
+    pFraction = 0.8;
+  } else if (RegExp(r'^[a-zA-z0-9]*$').hasMatch(password)) {
+    pFraction = 1.5;
+  } else if (RegExp(r'^[a-z]*$').hasMatch(password)) {
+    pFraction = 1.0;
+  } else if (RegExp(r'^[a-zA-z]*$').hasMatch(password)) {
+    pFraction = 1.3;
+  } else if (RegExp(r'^[a-z\- !?]*$').hasMatch(password)) {
+    pFraction = 1.3;
+  } else if (RegExp(r'^[a-z0-9]*$').hasMatch(password)) {
+    pFraction = 1.2;
+  } else {
+    pFraction = 1.8;
+  }
+  // ignore: prefer_function_declarations_over_variables
+  var logF = (double x) {
+    return 1.0 / (1.0 + exp(-x));
+  };
+  // ignore: prefer_function_declarations_over_variables
+  var logC = (double x) {
+    return logF((x / 2.0) - 4.0);
+  };
+
+  var pStrength = logC(password.length * pFraction);
+
+  return pStrength;
+}
+
+
 }
