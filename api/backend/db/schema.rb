19e0ae638c920caf822562a41ddd87c35560e0b5
Kevin <kevin.imbott@epitech.eu>
2022-05-12 14:27:35 +0200

    feat: add bdd schema

---
+++ api/backend/db/schema.rb
@@ -0,0 +1,60 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_05_12_121203) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "albums", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.bigint "picture_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["picture_id"], name: "index_albums_on_picture_id"
+    t.index ["user_id"], name: "index_albums_on_user_id"
+  end
+
+  create_table "albums_pictures", force: :cascade do |t|
+    t.bigint "picture_id"
+    t.bigint "album_id"
+    t.index ["album_id"], name: "index_albums_pictures_on_album_id"
+    t.index ["picture_id"], name: "index_albums_pictures_on_picture_id"
+  end
+
+  create_table "albums_users", id: false, force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "album_id"
+    t.index ["album_id"], name: "index_albums_users_on_album_id"
+    t.index ["user_id"], name: "index_albums_users_on_user_id"
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "session_token"
+    t.index ["email"], name: "unique_emails", unique: true
+  end
+
+end
