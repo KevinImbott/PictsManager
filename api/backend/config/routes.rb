8fdf36b38960723befcf6aebf5823874aa77eb3e
Kevin <kevin.imbott@epitech.eu>
2022-05-12 15:25:48 +0200

    feat: add user authentification with controller

--- api/backend/config/routes.rb
+++ api/backend/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  post "/login", to: "authentication#login"
+  post "/signup", to: "authentication#signup"
+
+  resources :users, only: [:new, :create]
+  resources :albums
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
