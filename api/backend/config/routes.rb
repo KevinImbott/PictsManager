223addbbb681cfa26d1e1b698e87f0af55a15639
Kevin <kevin.imbott@epitech.eu>
2022-05-13 15:35:46 +0200

    add pictures routes

--- api/backend/config/routes.rb
+++ api/backend/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :albums
+  resources :pictures
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
