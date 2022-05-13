529fe8f3067c038fbb313ee93674571200e59a62
Kevin <kevin.imbott@epitech.eu>
2022-05-13 16:57:14 +0200

    feat: add scope for user model

--- api/backend/app/controllers/users_controller.rb
+++ api/backend/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all
+    @users = User.by_recently_created
     render json: @users, status: :ok
   end
 
--- api/backend/app/models/user.rb
+++ api/backend/app/models/user.rb
@@ -9,4 +9,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :password, presence: true
   validates :pseudo, presence: true
+
+  scope :by_recently_created, -> { order(created_at: :desc).limit(20) }
+  scope :by_earliest_created, -> { order(created_at: :asc).limit(20) }
+  # Ex:- scope :active, -> {where(:active => true)}
 end
