3da14b511d010107d7ac9b4d40bb3837b8da3b3d
Kevin <kevin.imbott@epitech.eu>
2022-05-13 15:35:39 +0200

    feat: add pictures validations

--- api/backend/app/models/picture.rb
+++ api/backend/app/models/picture.rb
@@ -2,6 +2,11 @@ class Picture < ActiveRecord::Base
   has_and_belongs_to_many :albums
   has_many :users, through: :albums
 
+  has_one_attached :img
+
+  validates :name, presence: true
+  validates :description, presence: true
+
   def owner
     users.first
   end
