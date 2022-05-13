f9b3c1e763e631009a0bff590ca441b4e4bd906b
Kevin <kevin.imbott@epitech.eu>
2022-05-13 10:48:15 +0200

    feat: add index update destroy and profile method for user controller

--- api/backend/app/controllers/users_controller.rb
+++ api/backend/app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+    render json: @users, status: :ok
+  end
+
   def update
+    unless @current_user.update(permitted_params)
+      render json: { errors: @current_user.errors.full_messages },
+              status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @current_user.destroy
+  end
+
+  def profile
+    render json: @current_user, status: :ok
+  end
+
+  private
+
+  def permitted_params
+    params.permit([:email, :pseudo, :password])
   end
 end
