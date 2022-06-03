62c95b584c6052964215bc9427bfd789d92a33ce
Kevin <kevin.imbott@epitech.eu>
2022-06-03 12:23:20 +0200

    feat: add or delete user route for pictures

--- api/backend/config/routes.rb
+++ api/backend/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :users
   resources :albums
   post "/albums/:id/add_or_delete_user", to: "albums#add_or_delete_user"
+  post "/pictures/:id/add_or_delete_user", to: "pictures#add_or_delete_user"
   resources :pictures
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
