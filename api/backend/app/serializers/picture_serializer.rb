d28133149c1302059d0b2ce9dcfa813b03d5a043
Kevin <kevin.imbott@epitech.eu>
2022-06-07 23:42:43 +0200

    fix: rename other_user to invited users into picture serializer

--- api/backend/app/serializers/picture_serializer.rb
+++ api/backend/app/serializers/picture_serializer.rb
@@ -1,5 +1,11 @@
 class PictureSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :created_at, :updated_at
-  has_many :users
+  attributes :id, :name, :description, :created_at, :updated_at, :invited_users
+  # has_many :users
   has_many :albums
+
+  private
+
+  def invited_users
+    users.reject { |user| user == owner }
+  end
 end
