46a5ee7a3dd16dc2d9fc682dde8098e74b0d5e1d
Kevin <kevin.imbott@epitech.eu>
2022-06-09 11:12:00 +0200

    feat: add timestamp on album serializer

--- api/backend/app/serializers/album_serializer.rb
+++ api/backend/app/serializers/album_serializer.rb
@@ -1,5 +1,8 @@
 class AlbumSerializer < ActiveModel::Serializer
-  attributes :id, :name
-  has_many :users
+  attributes :id, :name, :updated_at, :created_at, :invited_users
   has_many :pictures
+
+  def invited_users
+    object.users.reject { |user| user == object.owner }
+  end
 end
