fa2144b2446c03f43876a5c3a8e211040a2a5259
Kevin <kevin.imbott@epitech.eu>
2022-06-09 09:52:05 +0200

    feat: add preview of pictures serializer

--- api/backend/app/serializers/picture_preview_serializer.rb
+++ api/backend/app/serializers/picture_preview_serializer.rb
@@ -1,13 +1,11 @@
 class PicturePreviewSerializer < ActiveModel::Serializer
-  attributes :id, :url
+  attributes :id, :name, :url
 
   include Rails.application.routes.url_helpers
 
   def url
     return unless object.img.key
 
-    variant = object.img.variant(resize: "100x100")
-    rails_representation_url(variant, only_path: true)
-    # ActiveStorage::Blob.service.path_for(object&.img&.key)
+    ActiveStorage::Blob.service.path_for(object&.img&.key)
   end
 end
