394e0506311a970d4ef86e50d22fce2c0c12e397
Kevin <kevin.imbott@epitech.eu>
2022-06-16 15:54:15 +0200

    feat: add view for albums in activeAdmin

--- api/backend/app/admin/albums.rb
+++ api/backend/app/admin/albums.rb
@@ -3,36 +3,22 @@ ActiveAdmin.register Album do
     attributes_table do
       row :id
       row :name
-      row :owner do |picture|
-        link_to picture.owner.pseudo, admin_user_path(picture.owner)
+      row :owner do |album|
+        link_to album.owner.pseudo, admin_user_path(album.owner)
       end
       row :created_at
       row :updated_at
       table_for album.pictures do
         column 'Pictures' do |picture|
-          link_to picture.name, admin_picture_path(picture.id)
+          img = image_tag url_for(picture.img)
+          link_to img, admin_picture_path(picture.id)
         end
       end
       table_for album.invited_users do
-        column 'Users' do |user|
+        column 'Invited Users' do |user|
           link_to user.pseudo, admin_user_path(user.id)
         end
       end
     end
   end
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :owner_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :owner_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
 end
