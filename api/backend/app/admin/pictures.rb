848106bd636d5a154e3953951ec09f4966dc9a62
Kevin <kevin.imbott@epitech.eu>
2022-06-16 16:32:09 +0200

    fix: requested changes

--- api/backend/app/admin/pictures.rb
+++ api/backend/app/admin/pictures.rb
@@ -26,19 +26,4 @@ ActiveAdmin.register Picture do
       end
     end
   end
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :description, :owner_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :description, :owner_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
 end
