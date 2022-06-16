2bfaa1e53267ef31c33fec32c9310f5bad5d2167
Kevin <kevin.imbott@epitech.eu>
2022-06-16 12:55:59 +0200

    feat: add picture view for active Admin

---
+++ api/backend/app/admin/pictures.rb
@@ -0,0 +1,34 @@
+ActiveAdmin.register Picture do
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :description
+      row :owner do |picture|
+        picture.owner.pseudo
+      end
+      row :image do |picture|
+        if picture.img.attached?
+          image_tag url_for(picture.img)
+        end
+      end
+      row :created_at
+      row :updated_at
+    end
+  end
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :description, :owner_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :description, :owner_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
