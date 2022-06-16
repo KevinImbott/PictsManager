5e55e2794a9b854106da9a0f46e512978020bca7
Kevin <kevin.imbott@epitech.eu>
2022-06-16 13:30:09 +0200

    feat:  finish pictures view

--- api/backend/app/admin/pictures.rb
+++ api/backend/app/admin/pictures.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Picture do
       row :name
       row :description
       row :owner do |picture|
-        picture.owner.pseudo
+        link_to picture.owner.pseudo, admin_user_path(picture.owner)
       end
       row :image do |picture|
         if picture.img.attached?
@@ -14,6 +14,18 @@ ActiveAdmin.register Picture do
       end
       row :created_at
       row :updated_at
+      table_for picture.users do
+        column 'Invited Users' do |user|
+          if user != picture.owner
+            link_to user.pseudo, admin_user_path(user.id)
+          end
+        end
+      end
+      table_for picture.albums do
+        column 'Albums' do |album|
+          link_to [album.id, album.name], admin_album_path(album.id)
+        end
+      end
     end
   end
   # See permitted parameters documentation:
