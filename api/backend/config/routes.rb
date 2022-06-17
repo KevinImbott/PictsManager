173847ca8d2bb4012e4036a5e5928ae65743a9ee
Kevin <kevin.imbott@epitech.eu>
2022-06-17 12:12:51 +0200

    feat: init picture route

--- api/backend/config/routes.rb
+++ api/backend/config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :albums
   post "/albums/:id/add_or_delete_user", to: "albums#add_or_delete_user"
   post "/pictures/:id/add_or_delete_user", to: "pictures#add_or_delete_user"
+  get "/pictures/home", to: "pictures#home"
   post "/pictures/:id/add_or_delete_album", to: "pictures#add_or_delete_album"
   resources :pictures
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
