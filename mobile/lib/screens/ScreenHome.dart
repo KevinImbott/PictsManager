1af3227dd39cb56b5b8aa1176de1e05941483982
william degre <william.degre@epitech.eu>
2022-06-24 15:16:14 +0200

    merge master to this branch

--- mobile/lib/screens/ScreenHome.dart
+++ mobile/lib/screens/ScreenHome.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../components/ImportButton.dart';
 import '../components/Navbar.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:pagination_view/pagination_view.dart';
 
 class ScreenHome extends StatefulWidget {
   const ScreenHome({ Key? key }) : super(key: key);
@@ -16,7 +17,35 @@ class _ScreenHome extends State<ScreenHome> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body:  PaginationView<Pic>(
+        preloadedItems: <Pic>[
+          Pic(faker.person.name(), faker.internet.email()),
+          Pic(faker.person.name(), faker.internet.email()),
+        ],
+        itemBuilder: (BuildContext context, Pic Pic, int index) => ListTile(
+          title: Text(user.name),
+          subtitle: Text(user.email),
+          leading: IconButton(
+            icon: Icon(Icons.person),
+            onPressed: () => null,
+          ),
+        ),
+        header: Text('Header text'),
+        footer: Text('Footer text'),
+        paginationViewType: PaginationViewType.listView // optional
+        pageFetch: pageFetch,
+        onError: (dynamic error) => Center(
+          child: Text('Some error occured'),
+        ),
+        onEmpty: Center(
+          child: Text('Sorry! This is empty'),
+        ),
+        bottomLoader: Center( // optional
+          child: CircularProgressIndicator(),
+        ),
+        initialLoader: Center( // optional
+          child: CircularProgressIndicator(),
+        ),
       ),
       bottomNavigationBar: const Navbar(),
       backgroundColor: const Color.fromRGBO(2, 2, 39, 1),
