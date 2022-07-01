dbd2f55e09e0ea91833d634dd82534cfc2c2f10d
Kevin <kevin.imbott@epitech.eu>
2022-07-01 14:16:47 +0200

    feat: add unicity constrait to pseudo

--- api/backend/db/schema.rb
+++ api/backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_16_094000) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_01_121500) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -103,6 +103,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_16_094000) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "unique_emails", unique: true
+    t.index ["pseudo"], name: "index_users_on_pseudo", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
