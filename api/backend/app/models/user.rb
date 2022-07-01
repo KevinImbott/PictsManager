1c181c6c98cb315875959795f59b3fef51f09d5a
Kevin <kevin.imbott@epitech.eu>
2022-07-01 14:09:56 +0200

    feat: add filter on users on index

--- api/backend/app/models/user.rb
+++ api/backend/app/models/user.rb
@@ -12,9 +12,6 @@ class User < ActiveRecord::Base
   validates :password, presence: true
   validates :pseudo, presence: true
 
-  scope :by_recently_created, -> { order(created_at: :desc) }
-  scope :by_earliest_created, -> { order(created_at: :asc) }
-
   def owned_albums
     Album.where(owner_id: id)
   end
